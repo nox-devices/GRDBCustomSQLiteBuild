e83f5196b843d0f9e2dbbd54fb71c6ee48d686c5
Dennis Müller <d3mueller@me.com>
2025-04-26 10:12:57 +0200

    Initial Commit

---
+++ GRDBCustomSQLiteBuild/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GRDBCustomSQLiteBuild
+//
+//  Created by Dennis Müller on 26.04.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GRDBCustomSQLiteBuild/GRDBCustomSQLiteBuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  GRDBCustomSQLiteBuildApp.swift
+//  GRDBCustomSQLiteBuild
+//
+//  Created by Dennis Müller on 26.04.25.
+//
+
+import SwiftUI
+
+@main
+struct GRDBCustomSQLiteBuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
